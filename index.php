a5cccb9fa755ed7417619eb44d4b52e7e2f3d8de
_bodya <bv@facility.team>
2016-11-29 20:25:17 +0200

    123

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo 'Hello World!' ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<pre>
+    <?php print_r($_SERVER) ?>
+</pre>
+</body>
+</html>
+
